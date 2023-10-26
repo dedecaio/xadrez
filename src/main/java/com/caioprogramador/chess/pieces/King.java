d28edc65a91576facb27332451ccba1f2086b589
Caio <caio.florees2005@gmail.com>
2023-10-26 15:48:37 -0300

    Creating and Placing pieces on the board

--- src/main/java/com/caioprogramador/chess/pieces/King.java
+++ src/main/java/com/caioprogramador/chess/pieces/King.java
@@ -1,4 +1,17 @@
 package com.caioprogramador.chess.pieces;
 
-public class King {
+import com.caioprogramador.boardgame.Board;
+import com.caioprogramador.chess.ChessPiece;
+import com.caioprogramador.chess.Color;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
 }
--- src/main/java/com/caioprogramador/chess/pieces/Rook.java
+++ src/main/java/com/caioprogramador/chess/pieces/Rook.java
@@ -1,4 +1,16 @@
 package com.caioprogramador.chess.pieces;
 
-public class Rook {
+import com.caioprogramador.boardgame.Board;
+import com.caioprogramador.chess.ChessPiece;
+import com.caioprogramador.chess.Color;
+
+public class Rook extends ChessPiece {
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
 }
