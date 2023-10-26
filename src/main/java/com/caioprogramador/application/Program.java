051d3358e5af270ebe814614d3baca03c11f9f1c
Caio <caio.florees2005@gmail.com>
2023-10-26 15:09:13 -0300

    Making the Board, Piece and Position.

--- src/main/java/com/caioprogramador/application/Program.java
+++ src/main/java/com/caioprogramador/application/Program.java
@@ -1,4 +1,9 @@
 package com.caioprogramador.application;
 
+import com.caioprogramador.boardgame.Position;
+
 public class Program {
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/com/caioprogramador/boardgame/Board.java
+++ src/main/java/com/caioprogramador/boardgame/Board.java
@@ -1,4 +1,30 @@
 package com.caioprogramador.boardgame;
 
 public class Board {
+
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.pieces = new Piece[columns][rows];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
 }
