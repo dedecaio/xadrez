f2e7dcdc93663d5261a18f24fe1040a39a7f89dd
Caio <caio.florees2005@gmail.com>
2023-10-31 15:46:00 -0300

    Switching player each turn

--- src/main/java/com/caioprogramador/application/UI.java
+++ src/main/java/com/caioprogramador/application/UI.java
@@ -1,5 +1,6 @@
 package com.caioprogramador.application;
 
+import com.caioprogramador.chess.ChessMatch;
 import com.caioprogramador.chess.ChessPiece;
 import com.caioprogramador.chess.ChessPosition;
 import com.caioprogramador.chess.Color;
@@ -46,6 +47,13 @@ public class UI {
         catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: "+chessMatch.getTurn());
+        System.out.println("Waiting player: "+chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
